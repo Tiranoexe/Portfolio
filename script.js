794cd65c976f4564b107e6992d69c373f4b09705
Tiranoexe <109442157+Tiranoexe@users.noreply.github.com>
2023-07-02 12:23:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var TopOffset = 85;    
+
+$('a[data-smooth]').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top - TopOffset
+          }, 1000);
+          return false;
+        }
+      }
+    });
